db19d97d6c8427f5371ad7e6488235a256dec70b
Aryan-yadav7 <yadavaryan1931@gmail.com>
2024-12-26 21:59:31 +0530

    Create practical6.py

---
+++ practical6.py
@@ -0,0 +1,28 @@
+## practical 6 - WAP to swap the first n characters of two strings .
+
+
+print("Enter the first string:", end="")
+
+string1 =input()
+
+print("Enter the second string:", end="")
+
+string2 =input()
+
+print("\nstring before swap: ")
+
+print(" string1 =", string1)
+
+print(" string2 =", string2)
+
+temp = string1 
+
+string1 = string2
+
+string2 = temp
+
+print("\nstring before swap: ")
+
+print(" string1 =", string1)
+
+print(" string2 =", string2)
